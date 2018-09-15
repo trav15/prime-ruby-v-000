6d49e63865eeff35931041c2c9b22ab410a5979b
Travis T <travistokuyama@gmail.com>
2018-09-15 15:38:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(int)
+  prime_nums = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+  if int == 2 
+    return true 
+  end
+  prime_nums.each do |prime_num|
+    if int == prime_num 
+      return true  
+    elsif int % prime_num == 0
+      return false
+    else
+      return true
+    end
+  end
+end
